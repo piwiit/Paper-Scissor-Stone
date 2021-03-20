5f07a7f2952e2216585ef44da351ba2126740771
piwiit <bouillaux@hotmail.fr>
2021-03-20 22:51:12 +0100

    rectif mistake background player choise

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// make random computer choise
 function computerPlay() {
   const arr = ['rock', 'paper', 'scissors'];
   const random = Math.floor(Math.random() * arr.length);
@@ -8,53 +9,67 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
   const result = document.querySelector('#result');
   const playerScoreShow = document.querySelector('#playerscore');
+
+  // focus buttons choise
   const computerScoreShow = document.querySelector('#computerscore');
   const computRockChoise = document.querySelector('#computerrock');
   const computerscissors = document.querySelector('#computerscissors');
   const computerpaper = document.querySelector('#computerpaper');
+  const playerRock = document.querySelector('#rock');
+  const playerPaper = document.querySelector('#paper');
+  const playerScissors = document.querySelector('#scissors');
 
+  //reset buttons color each round
   computerpaper.style.backgroundColor = '#555';
   computRockChoise.style.backgroundColor = '#555';
   computerscissors.style.backgroundColor = '#555';
 
+  playerRock.style.backgroundColor = '#555';
+  playerPaper.style.backgroundColor = '#555';
+  playerScissors.style.backgroundColor = '#555';
+
   if (playerSelection === computerSelection) {
     result.innerHTML = `draw you choise ${playerSelection} like computer`;
     document.querySelector(`#computer${computerSelection}`).style.backgroundColor = 'red';
+    document.querySelector(`#${playerSelection}`).style.backgroundColor = 'red';
   } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
     computerscissors.style.backgroundColor = 'red';
+    playerRock.style.backgroundColor = 'red';
     playerScore++;
     result.innerHTML = `You win, you choise ${playerSelection} computer choise ${computerSelection}`;
   } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
     computerpaper.style.backgroundColor = 'red';
+    playerScissors.style.backgroundColor = 'red';
     playerScore++;
     result.innerHTML = `You win, you choise ${playerSelection} computer choise ${computerSelection}`;
   } else if (playerSelection === 'paper' && computerSelection === 'rock') {
     computRockChoise.style.backgroundColor = 'red';
+    playerPaper.style.backgroundColor = 'red';
     playerScore++;
     result.innerHTML = `You win, you choise ${playerSelection} computer choise ${computerSelection}`;
   } else {
     computerScore++;
     document.querySelector(`#computer${computerSelection}`).style.backgroundColor = 'red';
+    document.querySelector(`#${playerSelection}`).style.backgroundColor = 'red';
     result.innerHTML = `computer win, you choise ${playerSelection} computer choise ${computerSelection}`;
   }
   playerScoreShow.innerHTML = `score : ${playerScore}`;
   computerScoreShow.innerHTML = `score : ${computerScore}`;
 }
 
+// init score
 let playerScore = 0;
 let computerScore = 0;
 
-let game = (playerSelection) => {
+const game = (playerSelection) => {
   const computerSelection = computerPlay();
   playRound(playerSelection, computerSelection);
-  console.log(playerSelection);
 };
 
 const buttons = document.querySelectorAll('.button');
 const startround = buttons.forEach((button) => {
   button.addEventListener('click', () => {
     playerSelection = button.id;
-    console.log(playerSelection);
     game(playerSelection);
   });
 });
